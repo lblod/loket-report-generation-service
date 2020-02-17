746e705358ca3582d908592bdba48e2a13403298
Oscar <lagartoverde97@gmail.com>
2020-02-17 09:41:38 +0100

    new way of importing reports, now crashes when it doesn't find index.js

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
 import { app, errorHandler } from 'mu';
 import cron from 'node-cron'
+import reports from './reports/index'
 
-try {
-  const reports = require('./reports/index.mjs')
-  reports.forEach(({cronPattern, execute}) => {
-    cron.schedule(cronPattern, execute)
-  })
-  app.get('/reports', async (req, res) => {
-    const reportName = req.query.name
-    if(reportName) {
-      let reportFound = false
-      await Promise.all(reports.forEach(async ({name, execute}) => {
-        if(reportName === name) {
-          reportFound = true
-          await execute()
-          res.send('Done ^^')
-          return
-        }
-      }))
-      if(!reportFound) {
-        return res.send(`There's no report named ${reportName}`)
+
+reports.forEach(({cronPattern, execute}) => {
+cron.schedule(cronPattern, execute)
+})
+
+app.get('/reports', async (req, res) => {
+  const reportName = req.query.name
+  if(reportName) {
+    let reportFound = false
+    for(let i = 0; i< reports.length; i++) {
+      const {name, execute} = reports[i]
+      if(reportName === name) {
+        reportFound = true
+        await execute()
+        res.send('Done ^^')
+        return
       }
-    }else {
-      return res.send('No report name')
     }
-  })
-} catch(e) {
-  app.get('/reports', (req, res) => {
-    console.log('no reports executed')
-    res.send('No reports found')
-  })
-}
+    if(!reportFound) {
+      return res.send(`There's no report named ${reportName}`)
+    }
+  }else {
+    return res.send('No report name')
+  }
+})
 
 app.get('/test', async (req, res) => {
   res.send('Hello World')
