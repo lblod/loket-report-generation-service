5026529a7f013cc0c46ec081b3191715f033bfab
Oscar <lagartoverde97@gmail.com>
2020-02-19 15:49:40 +0100

    converted to json api and cleaned

--- app.js
+++ app.js
@@ -4,27 +4,55 @@ import reports from './reports/index';
 
 
 reports.forEach(({cronPattern, execute}) => {
-  cron.schedule(cronPattern, execute);
+  if(cronPattern) {
+    try {
+      cron.schedule(cronPattern, execute);
+    }catch(e){
+      console.log(e)
+    }
+  }
 });
 
-app.get('/reports', async (req, res) => {
-  const reportName = req.query.name;
+app.post('/reports', async (req, res) => {
+  const reportName = req.body.name;
   if(reportName) {
-    let reportFound = false;
-    for(let i = 0; i< reports.length; i++) {
-      const {name, execute} = reports[i];
-      if(reportName === name) {
-        reportFound = true;
-        await execute();
-        res.send('Done ^^');
-        return;
-      }
-    }
-    if(!reportFound) {
-      return res.send(`There's no report named ${reportName}`);
+    let report = reports.find((report) => report.name === reportName)
+    if(report) {
+      await report.execute();
+      return res.json({
+        data: {
+          type: 'result',
+          id: 1,
+          attributes: {
+            label: 'success'
+          }
+        }
+      });
+    } else {
+      res.status(404);
+      return res.json({
+        data: {
+          type: 'result',
+          id: 2,
+          attributes: {
+            label: 'error',
+            info: `There's no report named ${reportName}`
+          }
+        }
+      });
     }
   }else {
-    return res.send('No report name');
+    res.status(400);
+    return res.json({
+      data: {
+        type: 'result',
+        id: 2,
+        attributes: {
+          label: 'error',
+          info: 'No report name specified in the request'
+        }
+      }
+    });
   }
 });
 
