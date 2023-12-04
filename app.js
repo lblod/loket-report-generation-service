8c258f50dd092033221e5d6a18ceba0ab2709c8d
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-12-04 13:27:32 +0100

    Fix report generation endpoint
    - Add `await` on `report.execute`
    - Handle error properly

--- app.js
+++ app.js
@@ -20,43 +20,47 @@ try {
   );
 }
 
-app.post('/reports', async (req, res) => {
-  const reportName = req.body.data.attributes.reportName;
-  if (reportName) {
-    let report = reports.find((report) => report.name === reportName);
-    if (report) {
-      report.execute();
-      return res.json({
-        data: {
-          type: 'report-generation-tasks',
-          attributes: {
-            status: 'success',
+app.post('/reports', async (req, res, next) => {
+  try {
+    const reportName = req.body.data.attributes.reportName;
+    if (reportName) {
+      let report = reports.find((report) => report.name === reportName);
+      if (report) {
+        await report.execute();
+        return res.json({
+          data: {
+            type: 'report-generation-tasks',
+            attributes: {
+              status: 'success',
+            },
           },
-        },
-      });
+        });
+      } else {
+        res.status(404);
+        return res.json({
+          data: {
+            type: 'report-generation-tasks',
+            attributes: {
+              status: 'error',
+              info: `There's no report named ${reportName}`,
+            },
+          },
+        });
+      }
     } else {
-      res.status(404);
+      res.status(400);
       return res.json({
         data: {
           type: 'report-generation-tasks',
           attributes: {
             status: 'error',
-            info: `There's no report named ${reportName}`,
+            info: 'No report name specified in the request',
           },
         },
       });
     }
-  } else {
-    res.status(400);
-    return res.json({
-      data: {
-        type: 'report-generation-tasks',
-        attributes: {
-          status: 'error',
-          info: 'No report name specified in the request',
-        },
-      },
-    });
+  } catch (e) {
+    next(e);
   }
 });
 
